eeb4da377d8e098b6d33b5c964ea0555e66e3f46
Haznijs21 <bombadourada1@gmail.com>
2022-02-28 15:23:08 +0200

    123

--- script.js
+++ script.js
@@ -1,46 +1,79 @@
 
+function izvadaTekstu() {
+    const m = nolasa();
+    if (m) {
+        m1 = m.m1;
+        m2 = m.m2;
+        m3 = m.m3;
+        teksts = rezultats(m1, m2, m3);
+        p = lielumi(m1,m2,m3);
+        if (teksts) {
+        } else {
+            
+        }
+        const sakne = document.getElementById("izvade");
+        const raksti = document.createElement("p");
+        sakne.innerHTML = teksts;
+        sakne.appendChild(raksti);
+    }
+}
+function lielumi(m1, m2, m3) {
+    console.log("Malas garumi: " + m1 + " " + m2 + " " + m3);
+
+    p = perimetrs(m1, m2, m3);
+    console.log("Perimetrs: " + p);
+
+    pusper = pusperimetrs(m1, m2, m3);
+    console.log("Pusperimetrs: " + pusper);
+
+    S = laukums(m1, m2, m3);
+    console.log("Laukums: " + S);
+    return {p, pusper, S};
+}
+
 function nolasa(m1, m2, m3) {
     let malas = document.getElementsByClassName("mala");
     m1 = parseFloat(mala1.value);
     m2 = parseFloat(mala2.value);
     m3 = parseFloat(mala3.value);
-    if (m1 > 0 && m2 > 0 && m3 > 0) {
-        console.log("Malas garumi: " + m1 + " " + m2 + " " + m3);
-        perimetrs(m1, m2, m3);
-        vaiVeidojas(m1,m2,m3);
-        return { m1, m2, m3 };
+    return { m1, m2, m3 };
+}
+function rezultats(m1, m2, m3) {
+    const t = "";
+    if (typeof m1 == 'number' && typeof m2 == 'number' && typeof m3 == 'number' && !isNaN(m1) && !isNaN(m2) && !isNaN(m3)) {
+        if (m1 > 0 && m2 > 0 && m3 > 0) {
+            if (m1 < m2 + m3 && m2 < m1 + m3 && m3 < m1 + m2) {
+                return true;
+            } else {
+                console.log("Trijstūris neveidojas");
+                t = "Trijstūris neveidojas";
+                return t;
+            }
+        } else {
+            console.log("Datiem jābūt nenegatīviem!");
+            t = "Datiem jābūt nenegatīviem!";
+            return "Datiem jābūt nenegatīviem!";
+        }
     } else {
-        console.log(false);
-        return false;
+        console.log("Datiem jābūt cipara veidā!");
+        t = "Datiem jābūt cipara veidā!";
+        return t;
     }
 }
 
 function perimetrs(m1, m2, m3) {
     let p = m1 + m2 + m3;
-    console.log("Perimetrs: " + p);
     return p;
 }
 
 function pusperimetrs(m1, m2, m3) {
     let pusper = (m1 + m2 + m3) / 2;
-    console.log("Pusperimetrs: " + pusper);
     return pusper;
 }
 
-function vaiVeidojas(m1, m2, m3) {
-    if(m1 >= m2+m3 || m2 >= m1+m3 || m3 >= m1+m2 ) {
-        console.log("Neveidojas");
-        return false;
-    } else {
-        console.log("Veidojas");
-        laukums(m1,m2,m3);
-        return true;
-    }
-}
 
-function laukums(m1,m2,m3) {
+function laukums(m1, m2, m3) {
     const pusper = pusperimetrs(m1, m2, m3);
-    let tlaukums = Math.sqrt(pusper * ((pusper-m1) * (pusper-m2) * (pusper-m3)));
-    console.log("Laukums: " + tlaukums);
+    let tlaukums = Math.sqrt(pusper * ((pusper - m1) * (pusper - m2) * (pusper - m3)));
     return tlaukums;
 }
